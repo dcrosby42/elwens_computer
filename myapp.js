bbdee6e5d1b3aec1a184152d25c35398068973ff
David Crosby <dcrosby@llnw.com>
2019-09-16 07:48:26 -0400

    more vue tutorial

--- myapp.js
+++ myapp.js
@@ -1,9 +1,24 @@
 export function onload() {
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>',
+  })
+
   const app = new Vue({
     el: '#myapp',
     data: {
-      main_title: 'ELWEN STATION 1',
-      message: 'Hello Vue!'
-    }
+      message: 'Hello Vue!',
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' },
+      ],
+    },
+    methods: {
+      reverseMessage: function() {
+        this.message = this.message.split('').reverse().join('')
+      },
+    },
   })
+  window.app = app
 }

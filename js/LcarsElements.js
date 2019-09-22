6fed0c68bbf5da780fb30825ae5e75edd5b531ef
David Crosby <dcrosby@llnw.com>
2019-09-22 01:22:28 -0400

    header elbow right

--- js/LcarsElements.js
+++ js/LcarsElements.js
@@ -1,29 +1,73 @@
 import ZoomGrid from "./ZoomGrid.js"
 
 const LcarsBase = {
+
   props: {
+
     color:{
       type: String,
-      default: "tan",
+      default: "",
     },
+
     fgColor:{
       type: String,
       default: "",
     },
+
+    width:{
+      type: Number,
+      default: 0,
+    },
+
+    height:{
+      type: Number,
+      default: 0,
+    },
   },
+
   methods: {
+
     bgColorClass: function() {
+      if (this.color == "") { return "" }
       return "lcars-" + this.color + "-bg"
     },
+
     fgColorClass: function() {
+      if (this.fgColor == "") { return "" }
       return "lcars-" + this.fgColor + "-color"
     },
+
+    sizeClass: function() {
+      if (this.height == 0 && this.width == 0) {
+        return ""
+      }
+      if (this.height == 0) {
+        return "lcars-u-" + this.width.toString()
+      }
+      if (this.width == 0) {
+        return "lcars-vu-" + this.height.toString()
+      }
+      return "lcars-u-"+this.width.toString()+"-"+this.height.toString()
+    },
+
+    appendBases: function(classes) {
+      return _.concat(classes,
+        this.sizeClass(),
+        this.bgColorClass(),
+        this.fgColorClass(),
+      )
+    },
+
   }
 }
 
 const LcarsElement = {
   extends: LcarsBase,
   props: {
+    color:{
+      type: String,
+      default: "tan",
+    },
     round:{
       type: String,
       default: "",
@@ -44,41 +88,47 @@ const LcarsElement = {
   },
   computed: {
     classList: function() {
-      return [
+      return this.appendBases([
         "lcars-element",
         this.rounding(),
-        this.bgColorClass(),
-        this.fgColorClass(),
-      ]
+      ])
     },
   },
   template: `<div :class="classList"><slot>NCC 1707-D</slot></div>`
 } 
 
+const LcarsRow = {
+  extends: LcarsBase,
+  computed: {
+    classList: function() {
+      return this.appendBases([
+        "lcars-row",
+      ])
+    },
+  },
+  template: `<div :class="classList"><slot></slot></div>`,
+}
+
 const LcarsColumn = {
   extends: LcarsBase,
+  computed: {
+    classList: function() {
+      return this.appendBases([
+        "lcars-column",
+      ])
+    },
+  },
+  template: `<div :class="classList"><slot></slot></div>`,
+}
+
+const LcarsColumn1 = {
+  extends: LcarsColumn,
   props: {
     width:{
       type: Number,
       default: 1,
     },
-  },
-  methods:{
-    widthClass: function() {
-      return "lcars-u-" + this.width.toString()
-    },
-  },
-  computed: {
-    classList: function() {
-      return [
-        "lcars-column",
-        this.bgColorClass(),
-        this.fgColorClass(),
-        this.widthClass(),
-      ]
-    },
-  },
-  template: `<div :class="classList"><slot></slot></div>`,
+  }
 }
 
 const TabLeft = {
@@ -101,9 +151,32 @@ const TabRight = {
   },
 }
 
+const HeaderElbowRight = {
+  extends: LcarsBase,
+  props: {
+    title: {
+      type: String,
+      default: "LCARS",
+    },
+  },
+  components: {
+    LcarsRow,
+  },
+  template: `
+    <lcars-row>
+      <div :class="appendBases(['lcars-bar', 'horizontal', 'left-end', 'decorated'])"></div>
+      <div :class="appendBases(['lcars-bar', 'horizontal', 'both-divider'])">{{ title }}</div>
+      <div :class="appendBases(['lcars-elbow', 'right-bottom'])"></div>
+    </lcars-row>
+  `
+}
+
 export default {
   LcarsElement,
+  LcarsRow,
   LcarsColumn,
+  LcarsColumn1,
+  HeaderElbowRight,
   TabLeft,
   TabRight,
   ZoomGrid,
--- js/PageSample1.js
+++ js/PageSample1.js
@@ -4,31 +4,22 @@ import LcarsElements from "./LcarsElements.js"
 export default {
   components: LcarsElements,
   template: `
-    <div class="lcars-row">
-
-      <div class="lcars-column lcars-u-10"> 
-        <div class="lcars-column lcars-u-10">
-          <div class="lcars-row">
-            <div class="lcars-bar horizontal left-end decorated lcars-lilac-bg"></div>
-            <div class="lcars-bar horizontal both-divider lcars-lilac-bg">DATABASE</div>
-            <div class="lcars-elbow right-bottom lcars-lilac-bg"></div>
-          </div>
-        </div>
-        <div class="lcars-row">
-          <lcars-column v-bind:width="5" fg-color="husk">
-            FSDK GJFH JKDFGHFDJKGHFDJ  FDSJ NGFDJ GFDNJGFDHJGDSH HG FSJGH DSJFHGSDFJJDSHGASD JHGDASJK GDSAHJ 
-            FSDK GJFH JKDFGHFDJKGHFDJ  FDSJ NGFDJ GFDNJGFDHJGDSH HG FSJGH DSJFHGSDFJJDSHGASD JHGDASJK GDSAHJ 
+    <lcars-row>
+      <lcars-column v-bind:width="10">
+        <header-elbow-right title="DATABASE" color="lilac" />
+        <lcars-row>
+          <lcars-column v-bind:width="5" v-bind:height="5" fg-color="husk">
             FSDK GJFH JKDFGHFDJKGHFDJ  FDSJ NGFDJ GFDNJGFDHJGDSH HG FSJGH DSJFHGSDFJJDSHGASD JHGDASJK GDSAHJ 
             FSDK GJFH JKDFGHFDJKGHFDJ  FDSJ NGFDJ GFDNJGFDHJGDSH HG FSJGH DSJFHGSDFJJDSHGASD JHGDASJK GDSAHJ 
             FSDK GJFH JKDFGHFDJKGHFDJ  FDSJ NGFDJ GFDNJGFDHJGDSH HG FSJGH DSJFHGSDFJJDSHGASD JHGDASJK GDSAHJ 
             FSDK GJFH JKDFGHFDJKGHFDJ  FDSJ NGFDJ GFDNJGFDHJGDSH HG FSJGH DSJFHGSDFJJDSHGASD JHGDASJK GDSAHJ 
             FSDK GJFH JKDFGHFDJKGHFDJ  FDSJ NGFDJ GFDNJGFDHJGDSH HG FSJGH DSJFHGSDFJJDSHGASD JHGDASJK GDSAHJ 
           </lcars-column>
-          <lcars-column v-bind:width="5">
+          <lcars-column v-bind:width="3">
             <zoom-grid>SENSOR GRID</zoom-grid>
           </lcars-column>
 				
-          <lcars-column />
+          <lcars-column1 />
           <lcars-column>
             <tab-left>PICARD</tab-left>
             <tab-left color="lilac"></tab-left>
@@ -36,10 +27,10 @@ export default {
             <tab-left color="danub">RIKER</tab-left>
             <tab-left>DATA</tab-left>
           </lcars-column> 
-        </div>
+        </lcars-row>
 
-      </div> 
+      </lcars-column> 
 
-    </div>
+    </lcars-row>
   `,
 }
